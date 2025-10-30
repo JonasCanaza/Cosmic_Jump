80d22b17e0bacbe17c7186ee4c61142fc8f528de
Jonas <jonascanaza@gmail.com>
2025-10-30 12:20:20 -0300

    Change the name of the game window

--- src/game/game_constants.h
+++ src/game/game_constants.h
@@ -6,7 +6,7 @@ namespace Game
 {
 	const int SCREEN_WIDTH = 1024;
 	const int SCREEN_HEIGHT = 768;
-	const std::string PROGRAM_NAME = "Spatium Carnis";
+	const std::string PROGRAM_NAME = "Cosmic Jump";
 	const std::string PROGRAM_VERSION = "0.1";
 	const int VERSION_TEXT_SIZE = 36;
 }
