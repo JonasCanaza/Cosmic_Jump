1b795fc43a4cf7d280d586be6958d7b7d09c1cd4
Jonas <jonascanaza@gmail.com>
2025-10-31 11:57:02 -0300

    Add the files for the game screens

---
+++ src/screens/credits_screen.cpp
@@ -0,0 +1,29 @@
+#include "credits_screen.h"
+
+namespace Credits
+{
+	void Init()
+	{
+
+	}
+
+	void Input()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void Draw()
+	{
+
+	}
+
+	void Close()
+	{
+
+	}
+}
---
+++ src/screens/main_menu_screen.cpp
@@ -0,0 +1,29 @@
+#include "main_menu_screen.h"
+
+namespace MainMenu
+{
+	void Init()
+	{
+
+	}
+
+	void Input()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void Draw()
+	{
+
+	}
+
+	void Close()
+	{
+
+	}
+}
