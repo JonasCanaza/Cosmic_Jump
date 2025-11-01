a87e4cc47ab61b7d35a0b3266b6f28ee28a45a5f
Jonas <jonascanaza@gmail.com>
2025-11-01 01:31:29 -0300

    Add a simple logo to the main menu

--- src/interface/ui_constants.h
+++ src/interface/ui_constants.h
@@ -7,4 +7,10 @@ namespace UIConstants
     const float BUTTON_WIDTH = 325.0f;
     const float BUTTON_HEIGHT = 75.0f;
     const float BUTTON_MARGIN_Y = 10.0f;
+
+    // LOGO
+
+    const float LOGO_WIDTH = 600.0f;
+    const float LOGO_HEIGHT = 350.0f;
+    const float LOGO_TO_BUTTONS_MARGIN = 40.0f;
 }
--- src/screens/main_menu_screen.cpp
+++ src/screens/main_menu_screen.cpp
@@ -3,6 +3,7 @@
 #include "raylib.h"
 
 #include "game/game.h"
+#include "geometry/rectangle.h"
 #include "interface/button.h"
 #include "game/game_constants.h"
 #include "interface/ui_constants.h"
@@ -12,16 +13,22 @@ using namespace UIConstants;
 
 namespace MainMenu
 {
+	static Geometry::Rectangle logo;
+
 	static const int MAX_BUTTONS = 3;
 	static Button::Button buttons[MAX_BUTTONS];
-	static std::string buttonNames[MAX_BUTTONS] = { "Play", "Credits", "Exit" };
+	static const std::string buttonNames[MAX_BUTTONS] = { "Play", "Credits", "Exit" };
 
+	static void InitLogo();
 	static void InitButtons();
+	static void DrawLogo();
 	static void DrawButtons();
 	static void UpdateButtons();
+	static float GetTotalMenuHeight();
 
 	void Init()
 	{
+		InitLogo();
 		InitButtons();
 	}
 
@@ -40,6 +47,7 @@ namespace MainMenu
 		ClearBackground(BLACK);
 		BeginDrawing();
 
+		DrawLogo();
 		DrawButtons();
 
 		EndDrawing();
@@ -50,19 +58,41 @@ namespace MainMenu
 
 	}
 
+	static void InitLogo()
+	{
+		float totalMenuHeight = GetTotalMenuHeight();
+		float logoStartY = (SCREEN_HEIGHT - totalMenuHeight) / 2.0f;
+
+		logo.x = static_cast<float>(SCREEN_WIDTH) / 2.0f - LOGO_WIDTH / 2.0f;
+		logo.y = logoStartY;
+		logo.width = LOGO_WIDTH;
+		logo.height = LOGO_HEIGHT;
+	}
+
 	static void InitButtons()
 	{
-		const float logoStartY = static_cast<float>(SCREEN_HEIGHT) / 2.0f;
-		const float buttonsStartY = logoStartY;
+		float totalMenuHeight = GetTotalMenuHeight();
+		float logoStartY = (SCREEN_HEIGHT - totalMenuHeight) / 2.0f;
+		float buttonsStartY = logoStartY + LOGO_HEIGHT + LOGO_TO_BUTTONS_MARGIN;
 
 		for (int i = 0; i < MAX_BUTTONS; i++)
 		{
-			const float buttonCenterX = SCREEN_WIDTH / 2.0f;
-			const float buttonX = buttonCenterX - BUTTON_WIDTH / 2.0f;
-			const float buttonY = buttonsStartY + (BUTTON_HEIGHT + BUTTON_MARGIN_Y) * i;
+			float buttonCenterX = logo.x + LOGO_WIDTH / 2.0f;
+			float buttonX = buttonCenterX - BUTTON_WIDTH / 2.0f;
+			float buttonY = buttonsStartY + (BUTTON_HEIGHT + BUTTON_MARGIN_Y) * i;
 
 			buttons[i] = Button::Create(buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT, buttonNames[i]);
 		}
+	}
+
+	static void DrawLogo()
+	{
+		int x = static_cast<int>(logo.x);
+		int y = static_cast<int>(logo.y);
+		int width = static_cast<int>(logo.width);
+		int height = static_cast<int>(logo.height);
+
+		DrawRectangle(x, y, width, height, WHITE);
 	}
 
 	static void DrawButtons()
@@ -79,5 +109,10 @@ namespace MainMenu
 		{
 			Button::Update(buttons[i]);
 		}
+	}
+
+	static float GetTotalMenuHeight()
+	{
+		return LOGO_HEIGHT + LOGO_TO_BUTTONS_MARGIN + (BUTTON_HEIGHT * MAX_BUTTONS) + (BUTTON_MARGIN_Y * (MAX_BUTTONS - 1));
 	}
 }
