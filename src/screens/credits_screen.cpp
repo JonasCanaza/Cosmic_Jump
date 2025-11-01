4824ad6a5d3b5b05583296903295e871f6f6bffe
Jonas <jonascanaza@gmail.com>
2025-11-01 02:05:34 -0300

    Add button to the credits screen

--- src/screens/credits_screen.cpp
+++ src/screens/credits_screen.cpp
@@ -3,12 +3,25 @@
 #include "raylib.h"
 
 #include "game/game.h"
+#include "interface/button.h"
+#include "game/game_constants.h"
+#include "interface/ui_constants.h"
+
+using namespace Game;
+using namespace UIConstants;
 
 namespace Credits
 {
+	static Button::Button button;
+	static const std::string buttonName = "Back";
+
+	static void InitButton();
+	static void UpdateButton();
+	static void DrawButton();
+
 	void Init()
 	{
-
+		InitButton();
 	}
 
 	void Input()
@@ -21,7 +34,7 @@ namespace Credits
 
 	void Update()
 	{
-
+		UpdateButton();
 	}
 
 	void Draw()
@@ -30,6 +43,7 @@ namespace Credits
 		BeginDrawing();
 
 		DrawText("Credits!", 0, 0, 24, WHITE);
+		DrawButton();
 
 		EndDrawing();
 	}
@@ -37,5 +51,24 @@ namespace Credits
 	void Close()
 	{
 
+	}
+
+	static void InitButton()
+	{
+		float x = static_cast<float>(SCREEN_WIDTH) / 2.0f - BUTTON_WIDTH / 2.0f;
+		float marginY = 10;
+		float y = static_cast<float>(SCREEN_HEIGHT) - BUTTON_HEIGHT - marginY;
+
+		button = Button::Create(x, y, BUTTON_WIDTH, BUTTON_HEIGHT, buttonName);
+	}
+
+	static void UpdateButton()
+	{
+		Button::Update(button);
+	}
+
+	static void DrawButton()
+	{
+		Button::Draw(button);
 	}
 }
