33be1b5eee3cc9dc64e550c825458f3c2e7647d0
Jonas <jonascanaza@gmail.com>
2025-11-01 01:16:37 -0300

    Add buttons to the main menu

---
+++ src/interface/ui_constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace UIConstants
+{
+    // BUTTON
+
+    const float BUTTON_WIDTH = 325.0f;
+    const float BUTTON_HEIGHT = 75.0f;
+    const float BUTTON_MARGIN_Y = 10.0f;
+}
--- src/screens/main_menu_screen.cpp
+++ src/screens/main_menu_screen.cpp
@@ -3,30 +3,36 @@
 #include "raylib.h"
 
 #include "game/game.h"
+#include "interface/button.h"
+#include "game/game_constants.h"
+#include "interface/ui_constants.h"
+
+using namespace Game;
+using namespace UIConstants;
 
 namespace MainMenu
 {
+	static const int MAX_BUTTONS = 3;
+	static Button::Button buttons[MAX_BUTTONS];
+	static std::string buttonNames[MAX_BUTTONS] = { "Play", "Credits", "Exit" };
+
+	static void InitButtons();
+	static void DrawButtons();
+	static void UpdateButtons();
+
 	void Init()
 	{
-
+		InitButtons();
 	}
 
 	void Input()
 	{
-		if (IsKeyPressed(KEY_ONE))
-		{
-			CosmicJump::currentScene = CosmicJump::Scenes::Gameplay;
-		}
 
-		if (IsKeyPressed(KEY_TWO))
-		{
-			CosmicJump::currentScene = CosmicJump::Scenes::Credits;
-		}
 	}
 
 	void Update()
 	{
-
+		UpdateButtons();
 	}
 
 	void Draw()
@@ -34,7 +40,7 @@ namespace MainMenu
 		ClearBackground(BLACK);
 		BeginDrawing();
 
-		DrawText("Main Menu!", 0, 0, 24, WHITE);
+		DrawButtons();
 
 		EndDrawing();
 	}
@@ -42,5 +48,36 @@ namespace MainMenu
 	void Close()
 	{
 
+	}
+
+	static void InitButtons()
+	{
+		const float logoStartY = static_cast<float>(SCREEN_HEIGHT) / 2.0f;
+		const float buttonsStartY = logoStartY;
+
+		for (int i = 0; i < MAX_BUTTONS; i++)
+		{
+			const float buttonCenterX = SCREEN_WIDTH / 2.0f;
+			const float buttonX = buttonCenterX - BUTTON_WIDTH / 2.0f;
+			const float buttonY = buttonsStartY + (BUTTON_HEIGHT + BUTTON_MARGIN_Y) * i;
+
+			buttons[i] = Button::Create(buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT, buttonNames[i]);
+		}
+	}
+
+	static void DrawButtons()
+	{
+		for (int i = 0; i < MAX_BUTTONS; i++)
+		{
+			Button::Draw(buttons[i]);
+		}
+	}
+
+	static void UpdateButtons()
+	{
+		for (int i = 0; i < MAX_BUTTONS; i++)
+		{
+			Button::Update(buttons[i]);
+		}
 	}
 }
