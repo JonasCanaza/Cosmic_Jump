c27ca55a8d06d36aab99a0727b114b06135abab2
Jonas <jonascanaza@gmail.com>
2025-11-02 18:11:29 -0300

    Change the version number from 0.1 to 0.2

--- src/game/game_constants.h
+++ src/game/game_constants.h
@@ -7,6 +7,6 @@ namespace Game
 	const int SCREEN_WIDTH = 1024;
 	const int SCREEN_HEIGHT = 768;
 	const std::string PROGRAM_NAME = "Cosmic Jump";
-	const std::string PROGRAM_VERSION = "0.1";
+	const std::string PROGRAM_VERSION = "0.2";
 	const int VERSION_TEXT_SIZE = 36;
 }
