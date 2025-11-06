71ba58a128a2a5ed90ba6649bf98525f0e250cdd
Jonas <jonascanaza@gmail.com>
2025-11-06 00:27:13 -0300

    Change the window resolution from 1024x768 to 1280x720

--- src/game/game_constants.h
+++ src/game/game_constants.h
@@ -4,8 +4,8 @@
 
 namespace Game
 {
-	const int SCREEN_WIDTH = 1024;
-	const int SCREEN_HEIGHT = 768;
+	const int SCREEN_WIDTH = 1280;
+	const int SCREEN_HEIGHT = 720;
 	const std::string PROGRAM_NAME = "Cosmic Jump";
 	const std::string PROGRAM_VERSION = "0.2";
 	const int VERSION_TEXT_SIZE = 36;
